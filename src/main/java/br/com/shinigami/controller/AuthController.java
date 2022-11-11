c42008c1fb6e928a7f9fd67c3f54dfbfc3faa1fa
JONAS DE LARA <jonasdelara_@Hotmail.com>
2022-11-11 17:00:06 -0300

    alteracao auth controller

--- src/main/java/br/com/shinigami/controller/AuthController.java
+++ src/main/java/br/com/shinigami/controller/AuthController.java
@@ -1,5 +1,7 @@
 package br.com.shinigami.controller;
 
+import br.com.shinigami.dto.funcionario.FuncionarioCreateDTO;
+import br.com.shinigami.dto.funcionario.FuncionarioDTO;
 import br.com.shinigami.dto.funcionario.LoginDTO;
 import br.com.shinigami.security.TokenService;
 import br.com.shinigami.service.FuncionarioService;
@@ -24,7 +26,6 @@ public class AuthController {
 
     private final FuncionarioService funcionarioService;
 
-    private final TokenService tokenService;
 
     @PostMapping
     public ResponseEntity<String> autenticar(@RequestBody @Valid LoginDTO loginDTO) {
@@ -35,5 +36,13 @@ public class AuthController {
         log.info("Token Gerado com sucesso.");
 
         return new ResponseEntity<>(token, HttpStatus.OK);
+    }
+
+    @PostMapping("/Cadastrar")
+    public ResponseEntity<FuncionarioDTO> create(@RequestBody @Valid FuncionarioCreateDTO funcionario){
+        log.info("Criando funcionario....");
+        FuncionarioDTO funcionarioDTO = funcionarioService.create(funcionario);
+        log.info("Criando funcionario....");
+        return new ResponseEntity<>(funcionarioDTO,HttpStatus.OK);
     }
 }
