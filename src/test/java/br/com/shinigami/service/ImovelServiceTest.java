4d9f5d7990febd673a6f082c8dbe67936db9b137
paulo-pssj <paulinhops99@gmail.com>
2022-11-19 21:29:28 -0300

    update: 100% imovelService

--- src/test/java/br/com/shinigami/service/ImovelServiceTest.java
+++ src/test/java/br/com/shinigami/service/ImovelServiceTest.java
@@ -1,13 +1,16 @@
 package br.com.shinigami.service;
 
+import br.com.shinigami.dto.RelatorioImovelEnderecoDTO;
 import br.com.shinigami.dto.cliente.ClienteDTO;
 import br.com.shinigami.dto.endereco.EnderecoDTO;
 import br.com.shinigami.dto.imovel.ImovelCreateDTO;
 import br.com.shinigami.dto.imovel.ImovelDTO;
+import br.com.shinigami.dto.page.PageDTO;
 import br.com.shinigami.entity.ClienteEntity;
 import br.com.shinigami.entity.EnderecoEntity;
 import br.com.shinigami.entity.ImovelEntity;
 import br.com.shinigami.entity.enums.Tipo;
+import br.com.shinigami.entity.enums.TipoImovel;
 import br.com.shinigami.exceptions.RegraDeNegocioException;
 import br.com.shinigami.repository.ImovelRepository;
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -20,15 +23,20 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ImovelServiceTest {
@@ -129,7 +137,7 @@ public class ImovelServiceTest {
     }
 
     @Test(expected = RegraDeNegocioException.class)
-    public void deveTestasUpdadeComException() throws RegraDeNegocioException{
+    public void deveTestarUpdadeComException() throws RegraDeNegocioException{
         ClienteEntity clienteEntity = getClienteLocador();
         EnderecoEntity endereco = getEnderecoEntity();
         ImovelCreateDTO imovelCreateDTO = getImovelCreateDTO(clienteEntity, endereco);
@@ -137,6 +145,8 @@ public class ImovelServiceTest {
         when(imovelRepository.findByIdImovelAndAtivo(anyInt(), any())).thenReturn(null);
 
         imovelService.update(1,  imovelCreateDTO);
+
+        verify(imovelRepository, times(1)).save(any());
 
     }
 
@@ -149,6 +159,8 @@ public class ImovelServiceTest {
         when(clienteService.findById(any())).thenReturn(clienteEntity);
 
         imovelService.alugarImovel(imovelEntity);
+
+        verify(imovelRepository, times(1)).save(any());
 
     }
 
@@ -164,6 +176,86 @@ public class ImovelServiceTest {
 
         imovelService.alugarImovel(imovelEntity);
 
+        verify(imovelRepository, times(1)).save(any());
+
+    }
+
+    @Test
+    public  void deveTestarDeleteComSucesso()throws RegraDeNegocioException{
+        Integer idImovel = 1;
+        ClienteEntity clienteEntity = getClienteLocador();
+        EnderecoEntity endereco = getEnderecoEntity();
+        ImovelEntity imovel = getImovelEntity(clienteEntity, endereco);
+
+        when(imovelRepository.findById(anyInt())).thenReturn(Optional.of(imovel));
+        when(imovelRepository.save(any())).thenReturn(imovel);
+
+        imovelService.delete(idImovel);
+
+        verify(imovelRepository, times(1)).save(any());
+    }
+
+    @Test
+    public void deveTestarListarImoveisDisponiveisComSucesso()throws RegraDeNegocioException{
+        ClienteEntity clienteEntity = getClienteLocador();
+        EnderecoEntity endereco = getEnderecoEntity();
+        ImovelEntity imovelEntity = getImovelEntity(clienteEntity, endereco);
+
+        List<ImovelEntity> lista = new ArrayList<>();
+        lista.add(imovelEntity);
+
+        when(imovelRepository.findAllByAlugadoAndAtivo(any(), any())).thenReturn(lista);
+
+        List<ImovelDTO> listaDTO = imovelService.listarImoveisDisponiveis();
+
+        assertEquals(1, listaDTO.size());
+    }
+
+    @Test
+    public void deveTestarListaRelatorioImovelEndereco(){
+        RelatorioImovelEnderecoDTO relatorio = getRelatorio();
+        List<RelatorioImovelEnderecoDTO>  lista = new ArrayList<>();
+        lista.add(relatorio);
+
+        when(imovelRepository.retornarRelatorioImovelEnderecoDTO(any())).thenReturn(lista);
+
+        List<RelatorioImovelEnderecoDTO> listaRelatorio = imovelService.relatorioImovelEndereco(1);
+
+        assertEquals(1, listaRelatorio.size());
+
+    }
+
+    @Test
+    public void deveTestarListComSucesso() throws RegraDeNegocioException{
+        Integer pagina = 1;
+        Integer tamanho = 1;
+
+        ClienteEntity clienteEntity = getClienteLocador();
+        EnderecoEntity endereco = getEnderecoEntity();
+        ImovelEntity imovelEntity = getImovelEntity(clienteEntity, endereco);
+        PageImpl<ImovelEntity> page = new PageImpl<>(List.of(imovelEntity));
+
+        when(imovelRepository.findAllByAtivo(any(), any())).thenReturn(page);
+
+        PageDTO<ImovelDTO> listaRetorno = imovelService.list(pagina);
+
+        assertEquals(1, listaRetorno.getQuantidadePaginas());
+        assertEquals(1, listaRetorno.getTotalElementos());
+    }
+
+    private  RelatorioImovelEnderecoDTO getRelatorio() {
+        RelatorioImovelEnderecoDTO relatorio = new RelatorioImovelEnderecoDTO();
+        relatorio.setIdCliente(1);
+        relatorio.setNome("Paulo");
+        relatorio.setEmail("test@gmail.com");
+        relatorio.setIdImovel(1);
+        relatorio.setTipoImovel(TipoImovel.APARTAMENTO);
+        relatorio.setValorMensal(2000);
+        relatorio.setCidade("Cotia");
+        relatorio.setEstado("São Paulo");
+        relatorio.setPais("Brasil");
+
+        return relatorio;
     }
 
 
